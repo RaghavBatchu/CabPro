7e1731a488cee27cf96c09d8db95af80027ff24f
RaghavBatchu <batchuraghavendra07@gmail.com>
2026-04-12 23:24:14 +0530

    add vercel url to server.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ const io = new Server(server, {
           "http://127.0.0.1:5173",
           "http://localhost:5174",
           "http://127.0.0.1:5174",
+          "https://cab-pro.vercel.app",
         ],
     credentials: true,
     methods: ["GET", "POST"],
